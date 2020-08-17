3a3938a8b5817aa7bd54dca9338af640c99f7c4f
Michele Santagiuliana Busellato <michelesantagiuliana@hotmail.it>
2020-08-17 20:33:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Whatsapp UI Clone
+
+Clone of the Whatsapp user interface.
+It is a UI that can be used as a starting point for an `Android\iOS app`.
+
+<p float="left">
+<img src="https://user-images.githubusercontent.com/21276996/90434363-30323f80-e0c5-11ea-9553-5ad9aa61b51a.jpg" width="300" />
+<img src="https://user-images.githubusercontent.com/21276996/90434365-32949980-e0c5-11ea-865f-097108212e8b.jpg" width="300" />
+<img src="https://user-images.githubusercontent.com/21276996/90434370-345e5d00-e0c5-11ea-936c-cddbc97870ae.jpg" width="300" />
+<img src="https://user-images.githubusercontent.com/21276996/90434376-36c0b700-e0c5-11ea-823a-c4740a7690cd.jpg" width="300" />
+</p>
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+* You have installed `Android 4.1 Jelly Bean or above` to run it on your mobile device.
+* You have installed `Android Studio v4.0 or above` to edit/compile it on your computer.
+* You have installed the latest version of `Dart/Flutter SDK` on your computer
+* You have a `<Windows/Linux/Mac>` computer.
+* You have read `LICENSE file`.
+
+## Installing Whatsapp UI Clone
+
+To check the code, follow these steps:
+1. Clone the repository
+2. Open it in Android Studio
+3. Run the app on your device
+
+## Using Whatsapp UI Clone
+
+1. Start the app
+
+## Contributing to Whatsapp UI Clone
+
+To contribute to Whatsapp UI Clone, follow these steps:
+1. Fork this repository.
+2. Create a branch: `git checkout -b <branch_name>`.
+3. Make your changes and commit them: `git commit -m '<commit_message>'`
+4. Push to the original branch: `git push origin <project_name>/<location>`
+5. Create the pull request.
+
+Alternatively see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## Contributors
+
+Code:
+Michele Santagiuliana Busellato
+
+## Contact
+
+If you want to contact me you can reach me at <michelesantagiuliana@hotmail.it>.
+
+## License
+
+This project uses the following license: MIT license.
---
+++ lib/main.dart
@@ -0,0 +1,307 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  ScrollController _scrollController;
+
+  void initState() {
+    _tabController = new TabController(length: 4, vsync: this);
+    _scrollController = new ScrollController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Whatsapp UI Clone',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: NestedScrollView(
+          controller: _scrollController,
+          headerSliverBuilder: (BuildContext context, bool InnerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                actions: <Widget>[
+                  Icon(Icons.search),
+                  Container(
+                    width: 20.0,
+                  ),
+                  Icon(Icons.more_vert),
+                  Container(
+                    width: 20.0,
+                  ),
+                ],
+                pinned: true,
+                floating: true,
+                centerTitle: false,
+                backgroundColor: Color(0xFF075e54),
+                title: Text('Whatsapp UI'),
+                bottom: TabBar(
+                    indicatorWeight: 5.0,
+                    indicatorColor: Colors.white,
+                    controller: _tabController,
+                    tabs: [
+                      Tab(
+                        child: Icon(Icons.photo_camera),
+                      ),
+                      Tab(
+                        text: 'CHATS',
+                      ),
+                      Tab(
+                        text: 'STATUS',
+                      ),
+                      Tab(
+                        text: 'CALLS',
+                      )
+                    ]),
+              )
+            ];
+          },
+          body: TabBarView(
+            controller: _tabController,
+            children: <Widget>[Text('camera'), Chats(), Status(), Calls()],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Chats extends StatefulWidget {
+  ChatsState createState() => ChatsState();
+}
+
+class ChatsState extends State<Chats> {
+  List<String> names = [
+    'alpha',
+    'beta',
+    'gamma',
+    'alpha',
+    'beta',
+    'gamma',
+    'alpha',
+    'beta',
+    'gamma',
+  ];
+
+  List<String> text = [
+    'hello',
+    'hiii',
+    'how are you?',
+    'hello',
+    'hiii',
+    'how are you?',
+    'hello',
+    'hiii',
+    'how are you?',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFF25d366),
+        child: Center(
+          child: Icon(Icons.message),
+        ),
+        onPressed: null,
+      ),
+      body: Container(
+        child: ListView.builder(
+            itemCount: names.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+                    ),
+                    title: Text(names[index]),
+                    subtitle: Text(text[index]),
+                    trailing: Text('10:00'),
+                  ),
+                  Divider()
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
+
+class Status extends StatefulWidget {
+  StatusState createState() => StatusState();
+}
+
+class StatusState extends State<Status> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFF25d366),
+        child: Icon(Icons.photo_camera),
+        onPressed: null,
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+            ),
+            title: Text('My status'),
+            subtitle: Text('Tap to add status update'),
+          ),
+          Container(
+            color: Colors.grey[300],
+            width: MediaQuery.of(context).size.width,
+            height: 20.0,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                'Recent updates',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+            ),
+            title: Text('Beta'),
+            subtitle: Text('Latest update'),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+            ),
+            title: Text('Alpha'),
+            subtitle: Text('Latest update'),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+            ),
+            title: Text('Jane'),
+            subtitle: Text('Latest update'),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+            ),
+            title: Text('Herbert'),
+            subtitle: Text('Latest update'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Calls extends StatefulWidget {
+  CallsState createState() => CallsState();
+}
+
+class CallsState extends State<Calls> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+          ),
+          title: Text('Phillip'),
+          subtitle: Row(
+            children: <Widget>[
+              Icon(
+                Icons.arrow_upward,
+                color: Colors.green,
+              ),
+              Text('Today, 13:45'),
+            ],
+          ),
+          trailing: Icon(
+            Icons.phone,
+            color: Color(0xFF075e54),
+          ),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+          ),
+          title: Text('Annabel'),
+          subtitle: Row(
+            children: <Widget>[
+              Icon(
+                Icons.arrow_downward,
+                color: Colors.red,
+              ),
+              Text('Today, 09:15'),
+            ],
+          ),
+          trailing: Icon(
+            Icons.videocam,
+            color: Color(0xFF075e54),
+          ),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+          ),
+          title: Text('Robert'),
+          subtitle: Row(
+            children: <Widget>[
+              Icon(
+                Icons.arrow_downward,
+                color: Colors.red,
+              ),
+              Text('Yesterday, 13:45'),
+            ],
+          ),
+          trailing: Icon(
+            Icons.phone,
+            color: Color(0xFF075e54),
+          ),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://media-exp1.licdn.com/dms/image/C4D35AQGR399Kjn_eeg/profile-framedphoto-shrink_200_200/0?e=1597780800&v=beta&t=q7kjyIiib1m9JzS-uhecdFuUiKSbopWJ5hZhmgUzijY'),
+          ),
+          title: Text('Jack'),
+          subtitle: Row(
+            children: <Widget>[
+              Icon(
+                Icons.arrow_upward,
+                color: Colors.green,
+              ),
+              Text('Yesterday, 07:30'),
+            ],
+          ),
+          trailing: Icon(
+            Icons.phone,
+            color: Color(0xFF075e54),
+          ),
+        ),
+      ],
+    );
+  }
+}
